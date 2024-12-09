b620e58cbd63db89a23aeb588d1491e7c4225079
moonster <115135514+msab2170@users.noreply.github.com>
2024-12-09 18:02:53 +0900

    Update UnitTest1.cs - Add file written confirm
    Update UnitTest1.cs - Add file written confirm

--- MsLoggerUniTest/UnitTest1.cs
+++ MsLoggerUniTest/UnitTest1.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Text.RegularExpressions;
 using MsLogger;
 using System.IO;
 
@@ -8,13 +9,19 @@ namespace UniTestForMSLog
     [TestClass]
     public class UnitTest1
     {
-        public readonly string APP_NAME = typeof(UnitTest1).Namespace;
-        private readonly string verbose = "verbose log";
-        private readonly string debug = "debug log";
-        private readonly string information = "information log";
-        private readonly string warning = "Warning log";
-        private readonly string error = "error log";
-        private readonly string fatal = "fatal log";
+        public static readonly string APP_NAME = typeof(UnitTest1).Namespace;
+        private readonly string _Verbose = "verbose log";
+        private readonly string _Debug = "debug log";
+        private readonly string _Information = "information log";
+        private readonly string _Warning = "Warning log";
+        private readonly string _Error = "error log";
+        private readonly string _Fatal = "fatal log";
+
+        private readonly int _LogFileSizeLimit = 5_120_000;
+        private readonly string _LogDirectory = APP_NAME + "_Logs";
+        private readonly string _LogFileName = $"{APP_NAME}-{DateTime.Now:yyyyMMdd}";
+        private readonly string _LogFilePath = Path.Combine(APP_NAME + "_Logs", $"{APP_NAME}-{DateTime.Now:yyyyMMdd}.log");
+        
 
         public UnitTest1()
         {
@@ -24,49 +31,63 @@ namespace UniTestForMSLog
                     .IsAdditionalErrorLogFileEnabled(true)                                          // 로그레빌이 Error이상 로그만 기록하는 로그파일을 추가 생성
                     .ErrorLogFileDirectory(APP_NAME + "_Error_Logs")                         // 로그레빌이 Error이상 로그만 기록하는 로그파일을 위치시킬 폴더의 경로 (절대/상대 경로)
                     .ErrorLogFileNameWithoutExtension(APP_NAME + "_Error")              // 로그레빌이 Error이상 로그만 기록하는 로그파일의 이름(확장자는 자동으로 추가되므로 기입해도 .xxx.log파일이 생성됨)
-                    .MinimumLogLevel(Log.LogLevel.Verbose)                                    // 기록할 최소로그레벨
+                    .MinimumLogLevel(LogLevel.Verbose)                                    // 기록할 최소로그레벨
                     .WriteConsole(true)                                                                 // 콘솔에도 보이게 할지 여부
-                    .LogFileSizeLimit(5_120_000)                                                     // 최대 로그파일 크기
+                    .LogFileSizeLimit(_LogFileSizeLimit)                                                     // 최대 로그파일 크기
                     .LogFileCountLimitEnabled(true)                                                 // 로그파일 갯수 제한 여부
                     .RollingCountLimit(30);                                                             // 제한할 로그파일 갯수
         }
-        /*
-            테스트는 파일이 생성된 파일 내 마지막 줄까지 확인하는 것이 바람직하나, 
-            여기서는 콘솔에 적힌 출력된 로그와 예상되는 라인을 비교하는 것으로 간소화 하였다.
-        */
 
-        
         /// <summary>
         /// Log.Verbose 테스트
         /// </summary>
         [TestMethod]
-        public void VerboseString()
+        public void VerboseStringConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Log.Verbose(verbose);
+                Log.Verbose(_Verbose);
 
                 var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains(verbose) && result.ToLower().Contains("[verbose]"));
-
+                Assert.IsTrue(result.Contains(_Verbose) && result.ToLower().Contains("[verbose]"));
             }
         }
+        [TestMethod]
+        public void VerboseStringFile()
+        {
+            Log.Verbose(_Verbose);
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains(_Verbose) && lines[lines.Length - 1].ToLower().Contains("[verbose]"));
+        }
+
+        
 
         /// <summary>
         /// /// Log.Verbose override (string.format) 테스트
         /// </summary>
         [TestMethod]
-        public void VerboseFormat()
+        public void VerboseFormatConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Log.Verbose(verbose + " {0}, {1}", "변수1", "변수2");
+                Log.Verbose(_Verbose + " {0}, {1}", "변수1", "변수2");
 
                 var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains($"{verbose} 변수1, 변수2") && result.ToLower().Contains("[verbose]"));
+                Assert.IsTrue(result.Contains($"{_Verbose} 변수1, 변수2") && result.ToLower().Contains("[verbose]"));
             }
+        }
+
+
+        [TestMethod]
+        public void VerboseFormatFile()
+        {
+            Log.Verbose(_Verbose + " {0}, {1}", "변수1", "변수2");
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains($"{_Verbose} 변수1, 변수2") && lines[lines.Length - 1].ToLower().Contains("[verbose]"));
         }
 
 
@@ -74,33 +95,52 @@ namespace UniTestForMSLog
         /// Log.Debug 테스트
         /// </summary>
         [TestMethod]
-        public void DebugString()
+        public void DebugStringConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Log.Debug(debug);
+                Log.Debug(_Debug);
 
                 var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains(debug) && result.ToLower().Contains("[debug]"));
+                Assert.IsTrue(result.Contains(_Debug) && result.ToLower().Contains("[debug]"));
 
             }
+        }
+
+        [TestMethod]
+        public void DebugStringFile()
+        {
+            Log.Debug(_Debug);
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains(_Debug) && lines[lines.Length - 1].ToLower().Contains("[debug]"));
         }
 
         /// <summary>
         /// /// Log.Debug override (string.format) 테스트
         /// </summary>
         [TestMethod]
-        public void DebugFormat()
+        public void DebugFormatConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Log.Debug(debug + " {0}, {1}", "변수1", "변수2");
+                Log.Debug(_Debug + " {0}, {1}", "변수1", "변수2");
 
                 var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains($"{debug} 변수1, 변수2") && result.ToLower().Contains("[debug]"));
+                Assert.IsTrue(result.Contains($"{_Debug} 변수1, 변수2") && result.ToLower().Contains("[debug]"));
             }
+        }
+
+
+        [TestMethod]
+        public void DebugFormatFile()
+        {
+            Log.Debug(_Debug + " {0}, {1}", "변수1", "변수2");
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains($"{_Debug} 변수1, 변수2") && lines[lines.Length - 1].ToLower().Contains("[debug]"));
         }
 
 
@@ -108,52 +148,133 @@ namespace UniTestForMSLog
         /// Log.Information 테스트
         /// </summary>
         [TestMethod]
-        public void InformationString()
+        public void InformationStringConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Log.Information(information);
+                Log.Information(_Information);
 
                 var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains(information) && result.ToLower().Contains("[information]"));
+                Assert.IsTrue(result.Contains(_Information) && result.ToLower().Contains("[information]"));
 
             }
         }
+
+        [TestMethod]
+        public void InformationStringFile()
+        {
+            Log.Information(_Information);
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains(_Information) && lines[lines.Length - 1].ToLower().Contains("[information]"));
+        }
+
 
         /// <summary>
         /// /// Log.Information override (string.format) 테스트
         /// </summary>
         [TestMethod]
-        public void InformationFormat()
+        public void InformationFormatConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Log.Information(information + " {0}, {1}", "변수1", "변수2");
+                Log.Information(_Information + " {0}, {1}", "변수1", "변수2");
 
                 var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains($"{information} 변수1, 변수2") && result.ToLower().Contains("[information]"));
+                Assert.IsTrue(result.Contains($"{_Information} 변수1, 변수2") && result.ToLower().Contains("[information]"));
             }
         }
 
+
+
+        [TestMethod]
+        public void InformationFormatFile()
+        {
+            Log.Information(_Information + " {0}, {1}", "변수1", "변수2");
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains($"{_Information} 변수1, 변수2") && lines[lines.Length - 1].ToLower().Contains("[information]"));
+        }
 
 
         /// <summary>
         /// Log.Debug 테스트
         /// </summary>
         [TestMethod]
-        public void WarningString()
+        public void WarningStringConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Log.Warning(warning);
+                Log.Warning(_Warning);
 
                 var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains(warning) && result.ToLower().Contains("[warning]"));
+                Assert.IsTrue(result.Contains(_Warning) && result.ToLower().Contains("[warning]"));
 
             }
+        }
+
+
+        [TestMethod]
+        public void WarningStringFile()
+        {
+            Log.Warning(_Warning);
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains(_Warning) && lines[lines.Length - 1].ToLower().Contains("[warning]"));
+        }
+
+        /// <summary>
+        /// /// Log.Debug override (string.format) 테스트
+        /// </summary>
+        [TestMethod]
+        public void WarningFormatConsole()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Log.Warning(_Warning + " {0}, {1}", "변수1", "변수2");
+
+                var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
+                Assert.IsTrue(result.Contains($"{_Warning} 변수1, 변수2") && result.ToLower().Contains("[warning]"));
+            }
+        }
+
+        [TestMethod]
+        public void WarningFormatFile()
+        {
+            Log.Warning(_Warning + " {0}, {1}", "변수1", "변수2");
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains($"{_Warning} 변수1, 변수2") && lines[lines.Length - 1].ToLower().Contains("[warning]"));
+        }
+
+        /// <summary>
+        /// Log.Debug 테스트
+        /// </summary>
+        [TestMethod]
+        public void ErrorStringConsole()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Log.Error(_Error);
+
+                var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
+                Assert.IsTrue(result.Contains(_Error) && result.ToLower().Contains("[error]")); // 로그 메시지가 포함되어 있는지 확인
+
+            }
+        }
+
+        [TestMethod]
+        public void ErrorStringFile()
+        {
+            Log.Error(_Error);
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains(_Error) && lines[lines.Length - 1].ToLower().Contains("[error]"));
         }
 
 
@@ -161,87 +282,134 @@ namespace UniTestForMSLog
         /// /// Log.Debug override (string.format) 테스트
         /// </summary>
         [TestMethod]
-        public void WarningFormat()
+        public void ErrorFormatConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Log.Warning(warning + " {0}, {1}", "변수1", "변수2");
+                Log.Error(_Error + " {0}, {1}", "변수1", "변수2");
 
                 var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains($"{warning} 변수1, 변수2") && result.ToLower().Contains("[warning]"));
+                Assert.IsTrue(result.Contains($"{_Error} 변수1, 변수2") && result.ToLower().Contains("[error]"));
             }
         }
 
+
+        [TestMethod]
+        public void ErrorFormatFile()
+        {
+            Log.Error(_Error + " {0}, {1}", "변수1", "변수2");
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains($"{_Error} 변수1, 변수2") && lines[lines.Length - 1].ToLower().Contains("[error]"));
+        }
 
 
         /// <summary>
         /// Log.Debug 테스트
         /// </summary>
         [TestMethod]
-        public void ErrorString()
+        public void FatalStringConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Log.Error(error);
+                Log.Fatal(_Fatal);
 
                 var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains(error) && result.ToLower().Contains("[error]")); // 로그 메시지가 포함되어 있는지 확인
+                Assert.IsTrue(result.Contains(_Fatal) && result.ToLower().Contains("[fatal]"));
 
             }
         }
+
+
+        [TestMethod]
+        public void FatalStringFile()
+        {
+            Log.Fatal(_Fatal);
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains(_Fatal) && lines[lines.Length - 1].ToLower().Contains("[fatal]"));
+        }
+
+
 
         /// <summary>
         /// /// Log.Debug override (string.format) 테스트
         /// </summary>
         [TestMethod]
-        public void ErrorFormat()
+        public void FatalFormatConsole()
         {
             using (var sw = new StringWriter())
             {
                 Console.SetOut(sw);
-                Log.Error(error + " {0}, {1}", "변수1", "변수2");
+                Log.Fatal(_Fatal + " {0}, {1}", "변수1", "변수2");
 
                 var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains($"{error} 변수1, 변수2") && result.ToLower().Contains("[error]"));
+                Assert.IsTrue(result.Contains($"{_Fatal} 변수1, 변수2") && result.ToLower().Contains("[fatal]"));
             }
+        }
+
+
+        [TestMethod]
+        public void FatalFormatFile()
+        {
+            Log.Fatal(_Fatal + " {0}, {1}", "변수1", "변수2");
+            string oldFile = ReadLastLineFromLogFile();
+            string[] lines = File.ReadAllLines(oldFile);
+            Assert.IsTrue(lines[lines.Length - 1].Contains($"{_Fatal} 변수1, 변수2") && lines[lines.Length - 1].ToLower().Contains("[fatal]"));
         }
 
 
 
 
         /// <summary>
-        /// Log.Debug 테스트
+        /// 원본 소스에 있는 함수로 용량한도 넘을시 괄호와 숫자가 붙으면 그 숫자 중 가장 큰값을 가져오는 함수
         /// </summary>
-        [TestMethod]
-        public void FatalString()
+        /// <param name="logDirectory"></param>
+        /// <param name="logFileName"></param>
+        /// <returns></returns>
+        private static int GetLastLogFileIndex(string logDirectory, string logFileName)
         {
-            using (var sw = new StringWriter())
+            int maxIndex = 0;
+            string pattern = $@"{logFileName} \(\d+\)\.log$"; // "(숫자).log" 패턴
+
+            // 기존 파일 목록을 가져와서 인덱스를 찾기
+            foreach (var file in Directory.GetFiles(logDirectory))
             {
-                Console.SetOut(sw);
-                Log.Fatal(fatal);
-
-                var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains(fatal) && result.ToLower().Contains("[fatal]"));
-
+                string fileName = Path.GetFileName(file);
+                if (Regex.IsMatch(fileName, pattern))
+                {
+                    // 정규 표현식으로 숫자 부분 추출
+                    var match = Regex.Match(fileName, @"\(\d+");
+                    if (match.Success)
+                    {
+                        if (int.TryParse(match.Value.Substring(1), out int index))
+                        {
+                            maxIndex = Math.Max(maxIndex, index);
+                        }
+                    }
+                }
             }
+
+            return maxIndex; // 다음 인덱스 반환
         }
 
-        /// <summary>
-        /// /// Log.Debug override (string.format) 테스트
-        /// </summary>
-        [TestMethod]
-        public void FatalFormat()
-        {
-            using (var sw = new StringWriter())
-            {
-                Console.SetOut(sw);
-                Log.Fatal(fatal + " {0}, {1}", "변수1", "변수2");
 
-                var result = sw.ToString().Trim(); // 콘솔에 출력된 내용 가져오기
-                Assert.IsTrue(result.Contains($"{fatal} 변수1, 변수2") && result.ToLower().Contains("[fatal]"));
+        private string ReadLastLineFromLogFile()
+        {
+            FileInfo file = new FileInfo(_LogFilePath);
+            string oldFile = _LogFilePath;
+            if (file.Exists && file.Length > _LogFileSizeLimit)
+            {
+                int fileIndex = GetLastLogFileIndex(_LogDirectory, _LogFileName);
+                oldFile = Path.Combine(_LogDirectory, $"{_LogFileName} ({++fileIndex}).log");
+                while (File.Exists(oldFile) && new FileInfo(oldFile).Length >= _LogFileSizeLimit)
+                {
+                    oldFile = Path.Combine(_LogDirectory, $"{_LogFileName} ({++fileIndex}).log");
+                }
             }
+            return oldFile;
         }
     }
 }
